b334bd5195c7d1857fe498aca39125d6c95612c6
Zhanat Uturova <526628@gmail.com>
2023-04-20 01:40:56 +0600

    Complete final task of third module

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first = number % 10;
+        int second = number % 100 / 10;
+        int third = number % 1000 / 100;
+        int fourth = number % 10000 / 1000;
+        System.out.println(first + second + third + fourth);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int x = number % 10 * 100 + number % 100 / 10 * 10 + number % 1000 / 100;
+        System.out.println(x);
     }
 }
